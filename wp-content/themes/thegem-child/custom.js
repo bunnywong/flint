8353976a66abc465f599df632fd6051fd89df787
BunnY <w@bunnywong.com>
2018-06-16 03:48:40 +0800

    update menu

--- wp-content/themes/thegem-child/custom.js
+++ wp-content/themes/thegem-child/custom.js
@@ -10,6 +10,10 @@ jQuery(document).ready(function ($) {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   })
 
+  // desktop - remove last menu item
+  $('.menu-item-cart').remove()
+
+  // inject 2 menu items for mobile
   let menuItems = `<li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:press@joycewang.com">PRESS</a></li>
                     <li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:info@joycewang.com">CONTACT</a></li>`;
   if (jQuery(document).width() < 768) {
