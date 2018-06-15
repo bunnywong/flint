71e95c244f262130d8f69728106a4f8aeb3c49fa
BunnY <w@bunnywong.com>
2018-06-16 03:10:59 +0800

    quick way to add custom mobile menu

--- wp-content/themes/thegem-child/custom.js
+++ wp-content/themes/thegem-child/custom.js
@@ -1,14 +1,19 @@
 jQuery(document).ready(function ($) {
   // mobile menu I/O
   $('#primary-navigation button').toggle(function () {
-    console.log('show menu');
     $('body').addClass('fixed')
   },function () {
-    console.log('hide menu');
     $('body').removeClass('fixed')
   })
 
   $('.js-scroll-to-top').on('click', function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   })
+
+  let menuItems = `<li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:press@joycewang.com">PRESS</a></li>
+                    <li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:info@joycewang.com">CONTACT</a></li>`;
+  if (jQuery(document).width() < 768) {
+    // inject custom menu items
+    jQuery('.menu-item-cart').after(menuItems)
+  }
 })
