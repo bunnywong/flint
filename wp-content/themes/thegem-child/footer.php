e3b9f4cd30a7dfeeb36efdcea87576cca902acca
BunnY <w@bunnywong.com>
2018-06-19 11:45:25 +0800

    restructure footer

--- wp-content/themes/thegem-child/footer.php
+++ wp-content/themes/thegem-child/footer.php
@@ -46,23 +46,9 @@
 			<footer id="footer-nav" class="site-footer">
 				<div class="container"><div class="row">
 
-					<div class="col-md-3 col-md-push-9">
-						<?php
-              get_sidebar('footer');
-							$socials_icons = array('facebook' => thegem_get_option('facebook_active'), 'linkedin' => thegem_get_option('linkedin_active'), 'twitter' => thegem_get_option('twitter_active'), 'instagram' => thegem_get_option('instagram_active'), 'pinterest' => thegem_get_option('pinterest_active'), 'googleplus' => thegem_get_option('googleplus_active'), 'stumbleupon' => thegem_get_option('stumbleupon_active'), 'rss' => thegem_get_option('rss_active'), 'vimeo' => thegem_get_option('vimeo_active'), 'youtube' => thegem_get_option('youtube_active'), 'flickr' => thegem_get_option('flickr_active'));
-							if(in_array(1, $socials_icons)) : ?>
-							<div id="footer-socials"><div class="socials inline-inside socials-colored">
-									<?php foreach($socials_icons as $name => $active) : ?>
-										<?php if($active) : ?>
-											<a href="<?php echo esc_url(thegem_get_option($name . '_link')); ?>" target="_blank" title="<?php echo esc_attr($name); ?>" class="socials-item"><i class="socials-item-icon <?php echo esc_attr($name); ?>"></i></a>
-										<?php endif; ?>
-									<?php endforeach; ?>
-									<?php do_action('thegem_footer_socials'); ?>
-							</div></div><!-- #footer-socials -->
-						<?php endif; ?>
-					</div>
+					<div class="col-sm-3 footer-left-wrapper"><div class="footer-site-info"><?php echo wp_kses_post(do_shortcode(nl2br(stripslashes(thegem_get_option('footer_html'))))); ?></div></div>
 
-					<div class="col-md-6">
+					<div class="col-sm-6">
 						<?php if(has_nav_menu('footer')) : ?>
 						<nav id="footer-navigation" class="site-navigation footer-navigation centered-box" role="navigation">
 							<?php wp_nav_menu(array('theme_location' => 'footer', 'menu_id' => 'footer-menu', 'menu_class' => 'nav-menu styled clearfix inline-inside', 'container' => false, 'depth' => 1, 'walker' => new thegem_walker_footer_nav_menu)); ?>
@@ -70,7 +56,21 @@
 						<?php endif; ?>
 					</div>
 
-					<div class="col-md-3 col-md-pull-9 footer-left-wrapper"><div class="footer-site-info"><?php echo wp_kses_post(do_shortcode(nl2br(stripslashes(thegem_get_option('footer_html'))))); ?></div></div>
+          <div class="col-sm-3">
+            <?php
+              get_sidebar('footer');
+              $socials_icons = array('facebook' => thegem_get_option('facebook_active'), 'linkedin' => thegem_get_option('linkedin_active'), 'twitter' => thegem_get_option('twitter_active'), 'instagram' => thegem_get_option('instagram_active'), 'pinterest' => thegem_get_option('pinterest_active'), 'googleplus' => thegem_get_option('googleplus_active'), 'stumbleupon' => thegem_get_option('stumbleupon_active'), 'rss' => thegem_get_option('rss_active'), 'vimeo' => thegem_get_option('vimeo_active'), 'youtube' => thegem_get_option('youtube_active'), 'flickr' => thegem_get_option('flickr_active'));
+              if(in_array(1, $socials_icons)) : ?>
+              <div id="footer-socials"><div class="socials inline-inside socials-colored">
+                  <?php foreach($socials_icons as $name => $active) : ?>
+                    <?php if($active) : ?>
+                      <a href="<?php echo esc_url(thegem_get_option($name . '_link')); ?>" target="_blank" title="<?php echo esc_attr($name); ?>" class="socials-item"><i class="socials-item-icon <?php echo esc_attr($name); ?>"></i></a>
+                    <?php endif; ?>
+                  <?php endforeach; ?>
+                  <?php do_action('thegem_footer_socials'); ?>
+              </div></div><!-- #footer-socials -->
+            <?php endif; ?>
+          </div>
 
 				</div></div>
 			</footer><!-- #footer-nav -->
