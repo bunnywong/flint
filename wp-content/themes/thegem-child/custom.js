1a5fe93ea52116110671e959c75ac84b023c45fd
BunnY <w@bunnywong.com>
2018-06-21 20:11:58 +0800

    custom close button

--- wp-content/themes/thegem-child/custom.js
+++ wp-content/themes/thegem-child/custom.js
@@ -14,9 +14,21 @@ jQuery(document).ready(function ($) {
   $('.menu-item-cart').remove()
 
   // inject 2 menu items for mobile
-  let menuItems = `<li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:press@joycewang.com">PRESS</a></li>
-                    <li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:info@joycewang.com">CONTACT</a></li>`;
   if ($(document).width() < 768) {
+    // inject custom menu
+    const menuItems = `<li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:press@joycewang.com">PRESS</a></li>
+                      <li id="menu-item-91" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-81 megamenu-first-element"><a target="_blank" href="mailto:info@joycewang.com">CONTACT</a></li>`;
     $('#menu-item-81').after(menuItems)
+
+    // add custom clost button
+    $('#primary-menu').after('<div class="closs-menu-button"></div>')
+    // click custom clost button trigger hide original one
+    $('.closs-menu-button').on('click', function () {
+      $('body.fixed .menu-toggle').trigger('click').hide()
+      setTimeout(function(){
+        // show original button again
+        $('.menu-toggle').show();
+      }, 500);
+    })
   }
 })
